ebd79961b8fd585c7518b88e2d1699c0d5ed2012
Shantanu sINGH <shaneyasingh13@gmail.com>
2019-01-12 10:47:46 +0530

    passport.js integrated DWIJ please dont push everything again and again one job done is one commit stop behaving like a first year kid

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -16,19 +16,11 @@ exports.login =(req,res) =>{
         res.send('error occured')
       }
       else {
-<<<<<<< HEAD
+
         if((login_db['Registerno']) == register_no && login_db['Password'] == password){
           passport.authenticate('local')(req, res, function(){
             res.render("../views/dashboard",{name:req.body.username})
         });
-=======
-          if((login_db['Registerno']) == register_no && login_db['Password'] == password){
-            res.render("../views/dashboard",{name:register_no})
-          }
-          else {
-            res.render("../views/login")
-          }
->>>>>>> a6d68f6aef69e60b21210ed23af58f2c216af6e3
       }
     };
 });
