64074a9fb3486a1c31c961d054d177e9f681edcd
Shantanu sINGH <shaneyasingh13@gmail.com>
2019-01-12 10:37:41 +0530

    passport.js integrated

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,28 +1,26 @@
 const mongoose =require('mongoose');
 const login =require('../models/login_details.model')
 var express=require('express')
+var passport = require('passport')
 exports.index =(req,res)=>{
   res.render("../views/login")
 }
 exports.login =(req,res) =>{
   register_no=req.body.username
   password =req.body.password
-  console.log(register_no)
-  console.log(password)
   login.findOne({
     Registerno:register_no
     })
     .exec(function(err, login_db) {
       if(err) {
         res.send('error occured')
-      } else {
-        if((login_db['Registerno']) == register_no && login_db['Password'] == password){
-          res.render("../views/dashboard",{name:register_no})
-
-        }
-        else {
-          res.redirect("/")
-                  }
       }
-    });
+      else {
+        if((login_db['Registerno']) == register_no && login_db['Password'] == password){
+          passport.authenticate('local')(req, res, function(){
+            res.render("../views/dashboard",{name:req.body.username})
+        });
+      }
+    };
+});
 }
